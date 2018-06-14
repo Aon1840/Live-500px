7c1fadec0209be17745fe0060afffebd629c6a4f
Aon1840 <aonattapon1840.ap@gmail.com>
2018-06-14 14:54:19 +0700

    getItemViewType

--- app/src/main/java/com/example/aon_attapon/live500px/adapter/PhotoListAdapter.java
+++ app/src/main/java/com/example/aon_attapon/live500px/adapter/PhotoListAdapter.java
@@ -3,6 +3,7 @@ package com.example.aon_attapon.live500px.adapter;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.example.aon_attapon.live500px.view.PhotoListItem;
 
@@ -23,14 +24,34 @@ public class PhotoListAdapter extends BaseAdapter{
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        PhotoListItem item;
-        if(convertView != null){
-            item = (PhotoListItem) convertView;
-        }else{
-            item = new PhotoListItem(parent.getContext());
-        }
+    public int getViewTypeCount() {
+        return 2;
+    }
 
-        return item;
+    @Override
+    public int getItemViewType(int position) {
+        return position % 2 == 0 ? 0 : 1;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(getItemViewType(position) == 0){
+            PhotoListItem item;
+            if(convertView != null){
+                item = (PhotoListItem) convertView;
+            }else{
+                item = new PhotoListItem(parent.getContext());
+            }
+
+            return item;
+        } else {
+            TextView item;
+            if(convertView != null)
+                item = (TextView) convertView;
+            else
+                item = new TextView(parent.getContext());
+            item.setText("position: "+position);
+            return item;
+        }
     }
 }
