c32e0b2f22dcc27673c6bcf1e0d018e53127a107
Aon1840 <aonattapon1840.ap@gmail.com>
2018-06-19 13:25:04 +0700

    Add animation to listView

--- app/src/main/java/com/example/aon_attapon/live500px/adapter/PhotoListAdapter.java
+++ app/src/main/java/com/example/aon_attapon/live500px/adapter/PhotoListAdapter.java
@@ -2,9 +2,12 @@ package com.example.aon_attapon.live500px.adapter;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import com.example.aon_attapon.live500px.R;
 import com.example.aon_attapon.live500px.dao.PhotoItemCollectionDao;
 import com.example.aon_attapon.live500px.dao.PhotoItemDao;
 import com.example.aon_attapon.live500px.manager.PhotoListManager;
@@ -13,6 +16,7 @@ import com.example.aon_attapon.live500px.view.PhotoListItem;
 public class PhotoListAdapter extends BaseAdapter {
 
     PhotoItemCollectionDao dao;
+    int lastPosition = -1; //Current position that I here
 
     public void setDao(PhotoItemCollectionDao dao) {
         this.dao = dao;
@@ -52,6 +56,13 @@ public class PhotoListAdapter extends BaseAdapter {
         item.setDescriptionText(dao.getUsername()+"\n"+dao.getCamera());
         item.setImageUrl(dao.getImageUrl());
 
+        if(position > lastPosition){
+            Animation anim = AnimationUtils.loadAnimation(parent.getContext(),
+                    R.anim.up_from_bottom);
+
+            item.startAnimation(anim);
+            lastPosition = position;
+        }
         return item;
     }
 }
